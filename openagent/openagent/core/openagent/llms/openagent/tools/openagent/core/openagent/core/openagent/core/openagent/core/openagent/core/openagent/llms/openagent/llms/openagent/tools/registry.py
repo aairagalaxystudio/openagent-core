a4b363d52f28e268636eb636410e9914a280a01d
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-01-30 18:36:04 +0530

    tools: add tool registry

---
+++ openagent/openagent/core/openagent/llms/openagent/tools/openagent/core/openagent/core/openagent/core/openagent/core/openagent/core/openagent/llms/openagent/llms/openagent/tools/registry.py
@@ -0,0 +1,16 @@
+from typing import Dict
+from openagent.tools.base import Tool
+
+
+class ToolRegistry:
+    def __init__(self):
+        self._tools: Dict[str, Tool] = {}
+
+    def register(self, tool: Tool):
+        self._tools[tool.name] = tool
+
+    def get(self, name: str) -> Tool | None:
+        return self._tools.get(name)
+
+    def list(self):
+        return list(self._tools.keys())
